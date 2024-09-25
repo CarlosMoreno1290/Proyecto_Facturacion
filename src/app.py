0d0a253cae9d4817955089a3c0b615b8e8ccdf8d
carlosmoreno1290 <camoreno@alimentoscarnicos.com.co>
2024-09-25 18:34:19 -0500

    avance proyecto

--- src/app.py
+++ src/app.py
@@ -16,3 +16,19 @@ def crear_producto():
 @app.route('/ver_productos')
 def ver_productos():
     return render_template('tabla_productos.html', titulo_pagina = 'Ver Productos')
+
+@app.route('/crear_vendedor')
+def crear_vendedor():
+    return render_template('formulario_crear_vendedor.html', titulo_pagina = 'Crear vendedor')
+
+@app.route('/ver_vendedor')
+def ver_vendedor():
+    return render_template('tabla_vendedor.html', titulo_pagina = 'Ver vendedor')
+
+@app.route('/crear_cliente')
+def crear_cliente():
+    return render_template('formulario_crear_cliente.html', titulo_pagina = 'Crear cliente')
+
+@app.route('/ver_cliente')
+def ver_cliente():
+    return render_template('tabla_cliente.html', titulo_pagina = 'Ver cliente')
