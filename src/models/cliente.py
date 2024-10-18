597ea2bac88a0b1f8e6734aaa9d9d9128866a3b4
Carlos Andres Moreno Cordoba <ACcamorenoc@ads.loc>
2024-10-18 14:49:24 -0500

    continuacion proyecto

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -5,10 +5,10 @@ from src.models import session, Base
 class Productos(Base):
     __tablename__ = 'productos'
     id = Column(Integer, prymary_key=True)
-    descripcion = Column(String(300), unique=True, nullable=False)
-    valor_unitario = Column(Float(10,8))
-    unidad_medida = Column(String(3), nullable=False)
-    cantida_stock = Column(Float(10,8))
+    descripcion_cliente = Column(String(300), unique=True, nullable=False)
+    codigo_cliente = Column(Float(10,8))
+    valor_unitario = Column(String(3), nullable=False)
+    cantida = Column(Float(10,8))
     categoria = Column(Integer, ForeignKey('categorias.id'), nullable=False)
 
     def __init__(self, descripcion, valor_unitario, unidad_medida, cantida_stock, categoria):
---
+++ src/models/vendedor.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from src.models import session, Base
+
+
+class vendedor(Base):
+    __tablename__ = 'vendedor'
+    id = Column(Integer, prymary_key=True)
+    nombre_completo = Column(String(300), unique=True, nullable=False)
+    email = Column(String(400))
+    telefono = Column(Float(10), nullable=False)
+    direccion = Column(String(100))
+    fecha_ingreso = Column(DateTime, nullable=False)
+
+    def __init__(self, nombre_completo, email, telefono, direccion, fecha_ingreso):
+        self.nombre_completo = nombre_completo
+        self.email = email
+        self.telefono = telefono
+        self.direccion = direccion
+        self.fecha_ingreso = fecha_ingreso
